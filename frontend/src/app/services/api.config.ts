23fc9424458596e78e03d26799ce34dc8a33f7c9
CassandraNobre <cassandralima15@hotmail.com>
2026-08-29 17:50:13 -0300

    telas/front

--- frontend/src/app/services/api.config.ts
+++ frontend/src/app/services/api.config.ts
@@ -1,1 +1,5 @@
-export const API_URL = 'https://inkstation-backend.onrender.com/api';
+const estaEmDesenvolvimento = ['localhost', '127.0.0.1'].includes(window.location.hostname);
+
+export const API_URL = estaEmDesenvolvimento
+	? '/api'
+	: 'https://inkstation-backend.onrender.com/api';
