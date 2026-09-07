f83009dc72cc006a9e3dc1b2f9fe964e9cf73efa
CassandraNobre <cassandralima15@hotmail.com>
2026-09-07 17:37:14 -0300

    fix: ajusta API_URL para caminho relativo

--- frontend/src/app/services/api.config.ts
+++ frontend/src/app/services/api.config.ts
@@ -1,5 +1,6 @@
 const estaEmDesenvolvimento = ['localhost', '127.0.0.1'].includes(window.location.hostname);
 
+// Alterado para usar o caminho relativo /api, assim funciona em qualquer lugar
 export const API_URL = estaEmDesenvolvimento
 	? 'http://localhost:3000/api'
-	: 'https://inkstation-backend.onrender.com/api';
+	: '/api';
